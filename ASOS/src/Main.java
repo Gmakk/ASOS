bae1530f38e3ce6622771a4c247c93af0a6b2791
Gmakk <gleb21410@yandex.ru>
2023-05-12 03:38:06 +0300

    Time comparison

--- ASOS/src/Main.java
+++ ASOS/src/Main.java
@@ -13,14 +13,38 @@
  */
 
 
-
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 public class  Main {
-    public static void main(String[] args) {
-        HandlerRegistry reg = new HandlerRegistry(10);//создаем регистр и указываем длину строки
-        for(int i=0;i<5;i++)//создаем N обработчиков
-            reg.addHandler(i+1);
-        System.out.println(reg);
-        reg.startProcess();
-        System.out.println(reg);
+    public static void main(String[] args){
+        try {
+            PrintWriter writer = new PrintWriter("Time comparison.txt");
+
+            //КОГДА ОБРАБОТЧИКОВ МЕНЬШЕ ЧЕМ ДЛИНА КОМАНДЫ
+
+            long time1 = System.currentTimeMillis();//получаем начальное время
+            HandlerRegistry reg1 = new HandlerRegistry(1000);//создаем регистр и указываем длину строки
+            for (int i = 0; i < 100; i++)//создаем N обработчиков
+                reg1.addHandler(i + 1);
+            System.out.println(reg1);
+            reg1.startProcess();
+            System.out.println(reg1);
+            writer.println("Time with lack of handlers:" + (System.currentTimeMillis() - time1));//сравниваем с конечным
+
+            //КОГДА ОБРАБОТЧКОВ ХВАТАЕТ НА КОМАНДУ
+
+            long time2 = System.currentTimeMillis();//получаем начальное время
+            HandlerRegistry reg2 = new HandlerRegistry(1000);//создаем регистр и указываем длину строки
+            for (int i = 0; i < 1000; i++)//создаем N обработчиков
+                reg2.addHandler(i + 1);
+            System.out.println(reg2);
+            reg2.startProcess();
+            System.out.println(reg2);
+            writer.println("Regular time:" + (System.currentTimeMillis() - time2));//сравниваем с конечным
+
+            writer.close();
+        }catch (FileNotFoundException e){
+            System.out.println("Incorrect file");
+        }
     }
 }
